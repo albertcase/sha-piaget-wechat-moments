0b5912a202b5776bf2004413190d44434e8e6447
Vincen Wen <vincen.wen@samesamechina.com>
2015-05-15 19:24:33 +0800

    no message

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ document.addEventListener('touchmove' , function (ev){
         var vidArr = ["m0151xu7izy","b001672wkoe","a0016gys8ct"];
         var vPic = ["../imgs/poster1.jpg","../imgs/poster2.jpg","../imgs/poster3.jpg"]
         var vText = ["","姚晨携手陈哲艺拍摄伯爵艺术大片<br />独家花絮抢先观看",""]
-        var ringTips = ["上转：观赏姚晨短片花絮<br />下转：开启全球广告大片","上转：观赏伯爵艺术短片<br />下转：开启全球广告大片","上转：观赏伯爵艺术短片<br />下转：开启姚晨短片花絮"]
+        var ringTips = ["上转：观赏姚晨短片花絮<br />下转：开启全球广告大片","上转：观赏伯爵艺术短片<br />下转：开启全球广告大片",""]
 
 		var player;
 		var videoWidth = document.body.clientWidth;
@@ -81,13 +81,12 @@ document.addEventListener('touchmove' , function (ev){
 
 
 		/* 戒指动画 */
-		var v = 1;
+		var v = 0;
 		
-
 		var animationListener = function(){
 			$(".routeAni").removeClass('up');
 			$(".routeAni").removeClass('down');
-			console.log('do something');
+			//console.log('do something');
 		}
 
 		var anim = document.getElementById("ring");
@@ -96,33 +95,46 @@ document.addEventListener('touchmove' , function (ev){
 		var l = $(".activeArea"),k = $("#home");
 
 		touch.on(l, 'swipeup', function(event){
-			  $(".routeAni").addClass('up');
-			  v++;
 
-			  if(v>vidArr.length-1){
-			  	v=0;
-			  };
-			  videoFun(v);
-		      event.preventDefault();
+			$(".routeAni").addClass('up');
+			if(v==2)return;
+			if(v==0){
+			  videoFun("1");
+			  v=1;
+			  $(".footer").show();
+			}else if(v==1){
+				videoFun("0");
+				v=0;
+				$(".footer").hide();
+			}else{
+			  videoFun("2");
+			  v=2;
+			  $(".footer").show();
+			}
+			
+		    event.preventDefault();
 		});
 
 		touch.on(l, 'swipedown', function(event){
-			  $(".routeAni").addClass('down');
-			  v--;
+			$(".routeAni").addClass('down');
 
-			  if(v<0){
-			  	v=2;
-			  };
-
-			  videoFun(v);
-		      event.preventDefault();
+			if(v==2)return;
+			if(v==0){
+			  videoFun("1");
+			  v=1;
+			}else{
+				videoFun("2");
+				v=2;
+			}
+			$(".footer").show();
+		    event.preventDefault();
 		});
 
 
 		/* home-ring */
 		var animationListener_home = function(){
 			$(".p_animate").removeClass('up');
-			videoFun(1);
+			videoFun(0);
 		    $(".container").hide().css({"opacity":0});
 		    $("#video").show().animate({"opacity":1},1200);
 		}
